496e47cbac1fc05076db401edf9bf91effebe833
TheKaoser <kaosermail@gmail.com>
2024-01-27 02:19:30 +0100

    sheep scapes shepherd

--- Assets/Scripts/SheepMovement.cs
+++ Assets/Scripts/SheepMovement.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class SheepMovement : MonoBehaviour
 {
+    public GameObject[] shepherds;
     float timeToNextAction;
     float currentTimeToNextAction;
-    Vector2 nextPosition;
+    Vector3 nextPosition;
     float speed = 5f;
 
     // Start is called before the first frame update
@@ -19,12 +20,17 @@ public class SheepMovement : MonoBehaviour
     void Update()
     {   
         currentTimeToNextAction += Time.deltaTime;
-        if (currentTimeToNextAction >= timeToNextAction)
+        if(currentTimeToNextAction >= timeToNextAction)
         {
             timeToNextAction = Random.Range(5f, 10f);
             currentTimeToNextAction = 0f;
             nextPosition = transform.position + new Vector3(Random.Range(-2f, 2f), Random.Range(-2f, 2f));
-        }   
+        }
+
+        if(Vector3.Distance(shepherds[0].transform.position, transform.position) < 3f)
+        {
+            nextPosition = Vector3.Normalize(transform.position - shepherds[0].transform.position) * 10f;
+        }
         GetComponent<Rigidbody2D>().MovePosition(Vector3.MoveTowards(transform.position, nextPosition, Time.deltaTime * speed));
     }
 }

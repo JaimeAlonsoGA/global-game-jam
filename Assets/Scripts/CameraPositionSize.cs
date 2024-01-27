c8427aa7fff70958d49e8e4949ea777dc24440aa
aljoba <alvarojordanbarrera@gmail.com>
2024-01-27 21:21:39 +0100

    camera position simplified

--- Assets/Scripts/CameraPositionSize.cs
+++ Assets/Scripts/CameraPositionSize.cs
@@ -6,15 +6,25 @@ public class CameraPositionSize : MonoBehaviour
 {
     public Transform Shepherd1;
     public Transform Shepherd2;
+    //private Vector3 currentScreenSpace;
+
+    public Vector3 PositionX { get; private set; }
+    public Vector3 PositionY { get; private set; }
 
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(new Vector3(Shepherd1.position.x, Shepherd1.position.y, -12) , new Vector3(Shepherd2.position.x, Shepherd2.position.y, -12), 0.5f);
+        PositionX = new Vector3(Shepherd1.position.x, Shepherd1.position.y, -1);
+        PositionY = new Vector3(Shepherd2.position.x, Shepherd2.position.y, -1);
+        transform.position = Vector3.Lerp(PositionX , PositionY, 0.5f);
+
+        //currentScreenSpace = new Vector3(Shepherd1.position.x, Shepherd1.position.y, 0);
+
     }
 }

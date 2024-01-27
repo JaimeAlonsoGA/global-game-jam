6fab125dfeea3e92bf7bd979c3e7218cc7f7998c
aljoba <alvarojordanbarrera@gmail.com>
2024-01-27 01:23:00 +0100

    move second player

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -12,13 +12,28 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
-            gameObject.transform.position += new Vector3(0f, 2.5f, 0) * Time.deltaTime;
-        if(Input.GetKey(KeyCode.D))
-            gameObject.transform.position += new Vector3(2.5f, 0f, 0) * Time.deltaTime;
-        if(Input.GetKey(KeyCode.A))
-            gameObject.transform.position += new Vector3(-2.5f, 0f, 0) * Time.deltaTime;
-        if(Input.GetKey(KeyCode.S))
-            gameObject.transform.position += new Vector3(0f, -2.5f, 0) * Time.deltaTime;
+        if (gameObject.CompareTag("Shepherd"))
+        {
+            if (Input.GetKey(KeyCode.W))
+                gameObject.transform.position += new Vector3(0f, 2.5f, 0) * Time.deltaTime;
+            if (Input.GetKey(KeyCode.D))
+                gameObject.transform.position += new Vector3(2.5f, 0f, 0) * Time.deltaTime;
+            if (Input.GetKey(KeyCode.A))
+                gameObject.transform.position += new Vector3(-2.5f, 0f, 0) * Time.deltaTime;
+            if (Input.GetKey(KeyCode.S))
+                gameObject.transform.position += new Vector3(0f, -2.5f, 0) * Time.deltaTime;
+        } 
+        
+        else {
+            if (Input.GetKey(KeyCode.UpArrow))
+                gameObject.transform.position += new Vector3(0f, 2.5f, 0) * Time.deltaTime;
+            if (Input.GetKey(KeyCode.RightArrow))
+                gameObject.transform.position += new Vector3(2.5f, 0f, 0) * Time.deltaTime;
+            if (Input.GetKey(KeyCode.LeftArrow))
+                gameObject.transform.position += new Vector3(-2.5f, 0f, 0) * Time.deltaTime;
+            if (Input.GetKey(KeyCode.DownArrow))
+                gameObject.transform.position += new Vector3(0f, -2.5f, 0) * Time.deltaTime;
+        }
+
     }
 }

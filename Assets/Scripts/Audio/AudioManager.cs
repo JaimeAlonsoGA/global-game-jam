0ab1a5feea6bedc859866c1f519f91b1d5a515d1
Jaime <alonsog.jaime@gmail.com>
2024-01-27 21:30:27 +0100

    sound for sheep and shepherd on random intervals

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-
+    private FMOD.Studio.EventInstance sheepInstace;
+    private FMOD.Studio.EventInstance shepherdInstance;
     public void PlaySound(string path)
     {
         FMODUnity.RuntimeManager.PlayOneShot(path, GetComponent<Transform>().position);
@@ -12,8 +13,14 @@ public class AudioManager : MonoBehaviour
     
     void Start()
     {
+        sheepInstace = FMODUnity.RuntimeManager.CreateInstance("event:/Sheep");
+        shepherdInstance = FMODUnity.RuntimeManager.CreateInstance("event:/Shepherd");
+
         PlaySound("event:/SoundtrackNoWolf");
         PlaySound("event:/ForestAmbience");
+        sheepInstace.start();
+        shepherdInstance.start();
+
     }
 
     void Update()

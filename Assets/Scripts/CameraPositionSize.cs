1910646a484a893f6aec0ba0d581af22d9f16e73
aljoba <alvarojordanbarrera@gmail.com>
2024-01-28 01:18:07 +0100

    border camera limit with border map

--- Assets/Scripts/CameraPositionSize.cs
+++ Assets/Scripts/CameraPositionSize.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CameraPositionSize : MonoBehaviour
 {
@@ -12,18 +15,21 @@ public class CameraPositionSize : MonoBehaviour
     public Vector3 PositionY { get; private set; }
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         PositionX = new Vector3(Shepherd1.position.x, Shepherd1.position.y, -1);
         PositionY = new Vector3(Shepherd2.position.x, Shepherd2.position.y, -1);
-        transform.position = Vector3.Lerp(PositionX , PositionY, 0.5f);
 
+        transform.position = Vector3.Lerp(PositionX, PositionY, 0.5f);
+        float x = Mathf.Clamp(transform.position.x, -39, 12.8f);
+        float y = Mathf.Clamp(transform.position.y, -26f, 36);
+        transform.position = new Vector3 (x, y);
+        
         //currentScreenSpace = new Vector3(Shepherd1.position.x, Shepherd1.position.y, 0);
 
     }

9a708275434b5e7c555946f3bf15336fb0087268
aljoba <alvarojordanbarrera@gmail.com>
2024-01-28 08:12:04 +0100

    instantiation of wolf when timer = 0

--- Assets/Timer.cs
+++ Assets/Timer.cs
@@ -1,10 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
     public float timeRemaining = 40;
+    public GameObject wolf;
+    private void Instantiate(GameObject wolf, Vector3 spawn)
+    {
+        throw new NotImplementedException();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +24,8 @@ public class Timer : MonoBehaviour
     {
         if (timeRemaining > 0)
             timeRemaining -= Time.deltaTime;
+        else {
+            Instantiate(wolf, new Vector3(0, 0, 0));
+        }
     }
 }

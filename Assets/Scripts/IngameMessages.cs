36c8409db75570d550935c90f487b3aa974d7a59
aljoba <alvarojordanbarrera@gmail.com>
2024-01-28 12:29:38 +0100

    commented code for the message

--- Assets/Scripts/IngameMessages.cs
+++ Assets/Scripts/IngameMessages.cs
@@ -15,10 +15,8 @@ public class IngameMessages : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //GetComponent<SafeArea>().totalSheep;
         //if (level1 == true)
-        //GameObject.FindWithTag("totalSheep");
-        //fence.GetComponent<int>();
-        //GetComponent<SafeArea>();
+        //if(GetComponent<SafeArea>().numberSheepInside >=  GetComponent<SafeArea>().totalSheep / 2 )
+        //.Show();
     }
 }
